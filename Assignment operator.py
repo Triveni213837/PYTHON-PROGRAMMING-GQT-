04308170323c24e9d193f2e4deb59b1e2d6db158
Triveni <talamarchana@gmail.com>
2026-08-07 14:49:28 +0530

    python.programming

---
+++ Assignment operator.py
@@ -0,0 +1,30 @@
+
+#'+='
+a=10
+print(a)
+a += 1
+print(a)
+
+#'-='
+b=30
+print(b)
+b = b-1
+print(b)
+
+#'*='
+c=40
+print(c)
+c=c*2
+print(c)
+
+#'/='
+d=50
+print(d)
+d=d/2
+print(d)
+
+#'%='
+e=60
+print(e)
+e=e%7
+print(e)
