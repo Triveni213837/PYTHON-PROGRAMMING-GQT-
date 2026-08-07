c40de533825502c255d4fd2a2c9a19195faee29d
Triveni <talamarchana@gmail.com>
2026-08-07 20:52:24 +0530

    python program

---
+++ Special operator.py
@@ -0,0 +1,24 @@
+#identity operator
+a = 10
+b = 20
+print(a is b)
+print(a is not b)
+
+
+a = 10
+b = 10
+print( a is b)
+print(a is not b)
+
+#membership operator
+s = 'Rama killed Ravana'
+print('i' in s)
+print('Rava' in s)
+print('Mama' in s)
+print('sita' not in s)
+
+#adding two numbers
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+c = a + b
+print('The sum is:', c)
