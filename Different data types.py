e9cfcadff079d46cafd7e4e834f0e14313eb1cee
Triveni <talamarchana@gmail.com>
2026-08-07 13:48:56 +0530

    updated files

--- Different data types.py
+++ Different data types.py
@@ -16,10 +16,32 @@ height=5.6
 print(type(height))
 
 
-
+#program-4
 print()
 percentage =57.799264
 print(percentage)
 print(type(percentage))
+
+
+#program-5
+print()
+ch = 'm'
+print(ch)
+print(type(ch))
+
+
+#program-6
+print()
+name ="ramu"
+print(name)
+print(type(name))
+
+
+#program-7
+name1 = "shyamu"
+print(name1)
+print(type(name1))
+
+
 
 
