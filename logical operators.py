1519f32c5f4ffdc3ab5232c3654f546773ef0111
Triveni <talamarchana@gmail.com>
2026-08-07 14:39:13 +0530

    pythonprogram

---
+++ logical operators.py
@@ -0,0 +1,25 @@
+10>20
+print(10>20)
+
+10==10
+print(10==10)
+
+20!=30
+print(20!=30)
+
+(10==10 or 20==30)
+print((10==10 or 20==30))
+
+(10==10 and 20==30)
+print((10==10 and 20==30))
+
+not(10==10)
+print(not(10==10))
+
+
+#program
+age = 21
+nationality = 'Indian'
+print(age>=18 and nationality=='Indian')
+print(age>=18 and nationality=='American')
+print(age>=18 or nationality=='American')
