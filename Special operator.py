cfcc6bf703e39241af71ae4f606073832a90c694
Triveni <talamarchana@gmail.com>
2026-08-07 21:01:31 +0530

    python program

--- Special operator.py
+++ Special operator.py
@@ -22,3 +22,14 @@ a = int(input('Enter first number: '))
 b = int(input('Enter second number: '))
 c = a + b
 print('The sum is:', c)
+
+#eval() function
+#program -1
+exp = eval('10+20*2+5')
+print('Result = ',exp)
+
+#program -2
+exp= input('Enter the expression: ')
+print('Result = ',eval(exp))
+
+
