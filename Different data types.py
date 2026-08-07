ec933e318710878354bd46c2f0107bad3a5d4f59
Triveni <talamarchana@gmail.com>
2026-08-07 11:59:54 +0530

    python programs

---
+++ Different data types.py
@@ -0,0 +1,25 @@
+#program-1
+age = 21
+print(age)
+print(type(age))
+
+
+#program-2
+a=35.678990
+print(a)
+print(type(a))
+
+
+#program-3
+print()
+height=5.6
+print(type(height))
+
+
+
+print()
+percentage =57.799264
+print(percentage)
+print(type(percentage))
+
+
