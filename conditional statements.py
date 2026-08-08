050cb488f423ed0ae233e96a1b8f0916113867aa
Triveni <talamarchana@gmail.com>
2026-08-08 22:00:47 +0530

    add built infuctions

---
+++ conditional statements.py
@@ -0,0 +1,11 @@
+#if
+Gender = input("enter your gender: ")
+if Gender == 'Male':
+    print('You are Male')
+
+ # Finding wheather given number is +ve or -ve
+ age = int(input('Enter any number: '))
+if age >= 0:
+    print(age,'is a positive number')
+else:
+    print(age,'is a negative number')
