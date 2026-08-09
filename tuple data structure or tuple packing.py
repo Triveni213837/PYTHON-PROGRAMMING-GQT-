656071848e1334e2337a49884c9c58cf3089b3d7
Triveni <talamarchana@gmail.com>
2026-08-09 09:11:12 +0530

    add tuple

---
+++ tuple data structure or tuple packing.py
@@ -0,0 +1,39 @@
+#eg-1
+t = ()
+print(t)
+print(type(t))
+
+#eg-2
+t = (10,20,30)
+print(t)
+print(type(t))
+
+#eg-3
+t = 10,20
+print(t)
+print(type(t))
+
+#eg-4
+t = 10
+print(t)
+print(type(t))
+
+#eg-5
+t = 10,
+print(t)
+print(type(t))
+
+#eg-6
+t = (100)
+print(t)
+print(type(t))
+
+#eg-7
+t =(10,)
+print(t)
+print(type(t))
+
+#tuple packing
+t = 10,20,30,40
+print(t)
+print(type(t))
