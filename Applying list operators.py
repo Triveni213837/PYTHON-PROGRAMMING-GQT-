09d9f5b8b0984da1f1936c1594368c5782117350
Triveni <talamarchana@gmail.com>
2026-08-09 08:48:00 +0530

    add list

---
+++ Applying list operators.py
@@ -0,0 +1,50 @@
+#applying concatenation operator ('+') on the list
+l1 = ['virat,rohit','dhoni']
+print('l1 = ',l1)
+l2 = ['suresh','kohli']
+print('l2 = ',l2)
+l3 = l1 + l2
+print('l1+l2= ',l3)
+
+
+#applying repetition operator ('*') on the list
+l1 = ['idly']
+print(l1)
+
+l2 = l1*5
+print(l2)
+
+
+#applying '==, and '!=' operator on the lists
+l1 = [10,20,30]
+l2 = [10,20,30]
+l3 = ['A','B']
+print(l1 == l2)
+print(l2 == l3)
+print(l2 = l3)
+
+#applying other relational operators(>,>=,<,<=)
+l1 = [10,20,30]
+l2 = [5,6,7]
+print(l1>l2)
+print(l1>=l2)
+print(l1<l2)
+print(l1<=l2)
+
+
+#applying membership operator (in,not in)on a list
+l1 =[10,20,30,40,50]
+print(l1)
+print(30 in l1)
+print(10 not in l1)
+print(555 in l1)
+print(555 not in l1)
+
+      
+
+
+
+
+    
+     
+      
