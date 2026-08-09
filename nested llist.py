4532317de08a5d6d5a331bd6e9b35b309ab09c8f
Triveni <talamarchana@gmail.com>
2026-08-09 08:59:01 +0530

    add

---
+++ nested llist.py
@@ -0,0 +1,14 @@
+l1 = [10,20,['a','b','c']]
+print(l1)
+
+print(l1[2][1])
+print(l1[2][0])
+
+#program-2
+l2 = [10,20,['a','b','c',[True,False]]]
+print(l2)
+
+print(l2[0][1])
+print(l2[2][1])
+print(l2[1][2])
+print(l2[1][1])
